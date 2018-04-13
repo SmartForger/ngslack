4d10d50c1f00b7e1f4daaad78c5d028a18d36133
fullstackdevs14 <fullstackdevs14>
2018-04-13 01:45:23 -0700

    Group material module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatSnackBarModule } from '@angular/material';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { MaterialModule } from '../shared/material.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MatButtonModule,
-    MatSnackBarModule
+    MaterialModule
   ],
   declarations: [HomeComponent]
 })
