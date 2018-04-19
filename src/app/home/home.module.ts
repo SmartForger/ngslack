a743f6505fb051e6fbe8a12eff385dfe371580f6
SmartForger <23446619+SmartForger@users.noreply.github.com>
2018-04-19 00:41:38 -0700

    Add sidebard

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TextareaAutosizeModule } from 'ngx-textarea-autosize';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModule } from '../shared/material.module';
 import { HomeComponent } from './home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarGroupComponent } from './components/sidebar-group/sidebar-group.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { MsgBlockComponent } from './components/msg-block/msg-block.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MaterialModule
+    MaterialModule,
+    TextareaAutosizeModule
   ],
-  declarations: [HomeComponent]
+  declarations: [
+    HomeComponent,
+    SidebarComponent,
+    SidebarGroupComponent,
+    DropdownComponent,
+    MessagesComponent,
+    MsgBlockComponent
+  ]
 })
 export class HomeModule { }
---
+++ src/app/home/models/index.ts
@@ -0,0 +1,9 @@
+export enum MenuDir {
+  Up = 1,
+  Down
+}
+
+export interface MenuItem {
+  id: string;
+  label: string;
+}
